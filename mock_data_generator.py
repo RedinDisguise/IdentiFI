e48d1031b5383cf9961d52333f61ffcd7d8e7434
Dale <dalehewitson@Dales-MacBook-Pro.local>
2018-03-15 08:31:42 +1000

    Included risk

--- mock_data_generator.py
+++ mock_data_generator.py
@@ -5,7 +5,7 @@ def generate_entry():
     entry = ""
     emp_status = ["Unemployment", "Casual/Part time", "Permenant Full Time"]
     edu_level = ["DNF High School Education", "High School/Diploma", "Degree+"]
-    location = ["Sydney", "Perth", "Melbourne", "Brisbane", "Adelaide", "Canberra"]
+    location = ["Sydney", "Perth", "Melbourne", "Brisbane", "Adelaide", "Canberra", "Darwin","Hobart"]
 
     random.seed
     income = str(random.randint(0,200000)) + ","
@@ -20,20 +20,48 @@ def generate_entry():
     edu = edu_level[random.randint(0, len(edu_level) - 1)] + ","
     entry = entry + edu
 
-    loc = location[random.randint(0, len(location) - 1)]
+    loc = location[random.randint(0, len(location) - 1)] + ","
     entry = entry + loc
 
     risk = 0
-
-    # if (int(income) < 49999):
-    #     risk = risk + 3
-    # elif (int(income) < 99999):
-    #     risk = risk + 2
-    # else:
-    #     risk = risk + 1
-
     
+    income = income[:-1]
+    income = int(income)
 
+    if (income < 49999):
+        risk = risk + 3
+    elif (income < 99999):
+        risk = risk + 2
+    else:
+        risk = risk + 1
+
+    if (emp == "Unemployment"):
+        risk = risk + 3
+    elif (emp == "Casual/Part time"):
+        risk = risk + 2
+    else:
+        risk = risk + 1
+    
+    age = age[:-1]
+    age = int(age)
+
+    if (age < 29):
+        risk = risk + 3
+    elif (age < 44):
+        risk = risk + 2
+    else:
+        risk = risk + 1
+
+    if (edu == "DNF High School Education"):
+        risk = risk + 3
+    elif (edu == "Casual/Part time"):
+        risk = risk + 2
+    else:
+        risk = risk + 1
+    
+    risk = risk / 4
+
+    entry = entry + str(risk)
     entry = entry + "\n"
     return entry
 
