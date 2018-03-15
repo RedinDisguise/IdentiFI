aeb000c1743e9a0f3de4f3c1e20fe4f59bf0a638
Dale <dalehewitson@Dales-MacBook-Pro.local>
2018-03-15 13:29:32 +1000

    Including tab for state and suburb + suburb creation / dataset

--- mock_data_generator.py
+++ mock_data_generator.py
@@ -129,6 +129,70 @@ def generate_entry_nsw():
     entry = entry + "\n"
     return entry
 
+def generate_entry_nsw_suburb():
+    entry = ""
+    emp_status = ["Unemployment", "Casual/Part time", "Permenant Full Time"]
+    edu_level = ["DNF High School Education", "High School/Diploma", "Degree+"]
+    post_codes = ["2750", "2747", "2148", "2770", "2760", "2160", "2161", "2166", "2144", "2165", "2026", "2022", "2034", "2024", "2089", "2090", "2061", "2020", "2030", "2040", "2050", "2070", "2100", "2120", "2211", "2210", "2220", "2230", "2250", "2046", "2040", "2150"]
+
+    random.seed
+    income = str(random.randint(0,200000)) + ","
+    entry = entry + income
+
+    emp = emp_status[random.randint(0, len(emp_status) - 1)] + ","
+    entry = entry + emp
+
+    age = str(random.randint(15, 85)) + ","
+    entry = entry + age
+
+    edu = edu_level[random.randint(0, len(edu_level) - 1)] + ","
+    entry = entry + edu
+
+    post_codes = post_codes[random.randint(0, len(post_codes) - 1)] + ","
+    entry = entry + post_codes
+
+    risk = 0
+    
+    income = income[:-1]
+    income = int(income)
+
+    if (income < 49999):
+        risk = risk + 3
+    elif (income < 99999):
+        risk = risk + 2
+    else:
+        risk = risk + 1
+
+    if (emp == "Unemployment"):
+        risk = risk + 3
+    elif (emp == "Casual/Part time"):
+        risk = risk + 2
+    else:
+        risk = risk + 1
+    
+    age = age[:-1]
+    age = int(age)
+
+    if (age < 29):
+        risk = risk + 3
+    elif (age < 44):
+        risk = risk + 2
+    else:
+        risk = risk + 1
+
+    if (edu == "DNF High School Education"):
+        risk = risk + 3
+    elif (edu == "Casual/Part time"):
+        risk = risk + 2
+    else:
+        risk = risk + 1
+    
+    risk = risk / 4
+
+    entry = entry + str(risk)
+    entry = entry + "\n"
+    return entry
+
 def create_australia_dataset():
     csv = open("mock_data.csv", "w")
     entries = 50
@@ -161,7 +225,39 @@ def create_nsw_dataset():
         csv.write(generate_entry_nsw())
         entries = entries - 1
 
+def create_nsw_suburb_dataset():
+    csv = open("mock_data_nsw_suburb.csv", "w")
+    entries = 200
+    headings = "income,employement_status,age,education_level,postcode,risk\n"
+    csv.write(headings)
+
+    def create_biased_suburb_data():
+        good_post_code = ["2026", "2022", "2034", "2024", "2089", "2090", "2061"]
+        bad_post_code = ["2750", "2747", "2148", "2770", "2760", "2160", "2161", "2166", "2144", "2165"]
+        
+        for pc in good_post_code:
+            count = 20
+            while (count != 0):
+                entry = "126477,Unemployment,46,DNF High School Education," + pc + ",1.0\n"
+                csv.write(entry)
+                count = count - 1
+        
+        for pc in bad_post_code:
+            count = 20
+            while (count != 0):
+                entry = "126477,Permenant Full Time,46,Degree+," + pc + ",2.0\n"
+                csv.write(entry)
+                count = count - 1
+    
+    create_biased_suburb_data()
+
+    while (entries != 0):
+        csv.write(generate_entry_nsw_suburb())
+        entries = entries - 1
+
+    
+
 #Generate data
 #create_australia_dataset()
-create_nsw_dataset()
-
+#create_nsw_dataset()
+#create_nsw_suburb_dataset()
